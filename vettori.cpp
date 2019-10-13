9f64accdc4427b71d572129595972609f9309d6e
pasdm08 <pdm5843@gmail.com>
2019-10-13 12:44:45 +0200

    aggiunto media

--- vettori.cpp
+++ vettori.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
 	
 	float v[10];
+	float m=0;
 	int i;
 	
 	for(i=0; i<10; i++){
@@ -14,16 +15,11 @@ int main() {
     }
     cout << "voti: " << endl;
 	for(i=0; i<10; i++){
-	cout << v[i] << endl;	
+	cout << v[i] << endl;
+	m = ( m + v[i] )/10;
 	}
-   
+    cout << "la media dei voti e': " << m << endl;
 	 
-	
-	
-	
-	
-	
-	
 	
 	
 }
