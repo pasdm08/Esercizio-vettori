0cce31e6328e1e4443b2084296645c0e446e9114
pasdm08 <pdm5843@gmail.com>
2019-10-13 12:30:22 +0200

    array

--- vettori.cpp
+++ vettori.cpp
@@ -1,13 +1,23 @@
-#include<iosteram> 
+#include <iostream> 
+
 
 using namespace std;
 
-int main()
-{
+int main() {
 	
 	float v[10];
 	int i;
 	
+	for(i=0; i<10; i++){
+    cout << "inserisci voti" << endl;
+    cin >> v[i];
+    }
+    cout << "voti: " << endl;
+	for(i=0; i<10; i++){
+	cout << v[i] << endl;	
+	}
+   
+	 
 	
 	
 	
